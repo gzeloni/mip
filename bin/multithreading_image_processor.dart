4f398d1e89bcba0455f49bc6032a35aa7334ffbb
gzeloni <gustavoluizdejesus@gmail.com>
2023-04-29 11:10:15 -0300

    fix names of functions

--- bin/multithreading_image_processor.dart
+++ bin/multithreading_image_processor.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
-import 'package:multithreading_image_processor/black_and_white.dart';
+// import 'package:multithreading_image_processor/black_and_white.dart';
 import 'package:multithreading_image_processor/words_spliter.dart';
 
 void main() {
-  final Mip mip = Mip();
+  // final Mip mip = Mip();
   final WordsSplitter wordsSplitter = WordsSplitter();
   // mip.mip();
   stdout.write("> ");
   String? read = stdin.readLineSync();
-  wordsSplitter.split_words(read.toString());
+  wordsSplitter.splitWords(read.toString());
 }
--- lib/words_spliter.dart
+++ lib/words_spliter.dart
@@ -1,7 +1,7 @@
 import 'package:multithreading_image_processor/black_and_white.dart';
 
 class WordsSplitter {
-  List<String> split_words(String words) {
+  List<String> splitWords(String words) {
     List<String> result = words.split(" ");
     if (result.contains("filtro") && result.contains("p&b")) {
       Mip mip = Mip();
