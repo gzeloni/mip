e410f4d83c713c97e625e49f75e0b7b0a618bc30
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2023-04-30 17:42:22 -0300

    bug fix

--- lib/utils/commands_list.dart
+++ lib/utils/commands_list.dart
@@ -1,9 +1,8 @@
 String commands = '''
-  &make <link> parameters (the link can be added anywhere in the command)
-  p&b: Apply a black and white filter to the image
-  inverted: Apply an inverted color filter to the image
-  billboard: Apply a billboard (the image is made up of dots) filter to the image
-  sepia: Apply a sepia (shades of yellow) filter to the image
-  with vignette: Apply a vignette to the image (add a custom value as the last sentence of the command)
-'''
-    .trim();
+              &make <link> parameters (the link can be added anywhere in the command)\n
+              p&b: Apply a black and white filter to the image\n
+              inverted: Apply a inverted color filter to the image\n
+              billboard: Apply a billboard (the image is made up of dots) filter to the image\n
+              sepia: Apply a sepia (shades of yellow) filter to the image\n
+              with vignette: Apply a vignette to the image (add a custom value as the last sentence of the command)
+            ''';
