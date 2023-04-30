89626cf21869165afe86dcde7c3f3d622fde0fa8
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2023-04-30 17:33:22 -0300

    using ImageProcessing static methods

--- lib/mip.dart
+++ lib/mip.dart
@@ -5,36 +5,35 @@ import 'package:multithreading_image_processor/functions.dart';
 import 'package:http/http.dart' as http;
 
 class Mip {
-  List<String> words;
+  final List<String> words;
   Mip({required this.words});
 
   Future<void> mip(String imageLink, String filename) async {
     try {
-      // Caminho para a imagem a ser processada.
-      // const imagePath = 'assets/lena.png';
       final response = await http.get(Uri.parse(imageLink));
-      // Carrega a imagem do disco.
       final bytes = response.bodyBytes;
       final image = img.decodeImage(bytes);
-      // Cria um canal de comunicação para enviar a imagem para o isolate.
+
       final receivePort = ReceivePort();
-      // Cria um isolate e envia o canal de comunicação como argumento.
       await Isolate.spawn(
         imageProcessor,
-        {'sendPort': receivePort.sendPort, 'image': image},
+        {
+          'sendPort': receivePort.sendPort,
+          'image': image,
+          'words': words,
+        },
       );
-      // Aguarda o recebimento da mensagem contendo a imagem processada.
       final newImage = await receivePort.first as img.Image;
-      // Salva a imagem processada no disco.
       await File(filename).writeAsBytes(img.encodePng(newImage));
     } catch (e) {
       print(e.toString());
     }
   }
 
-  void imageProcessor(Map<String, dynamic> args) async {
-    // Obtém a imagem enviada pelo processo principal.
+  static void imageProcessor(Map<String, dynamic> args) async {
     final image = args['image'] as img.Image;
+    final words = args['words'] as List<String>;
+
     bool shouldApplyBlackAndWhite =
         words.contains('p&b') && words.every((e) => e != 'inverted');
     bool shouldApplyInvertedColor =
@@ -46,46 +45,37 @@ class Mip {
     bool shouldApplyBillboard = words.contains('billboard');
     bool shouldApplySepia = words.contains('sepia');
     bool shouldApplyBulge = words.contains('bulge');
+
     img.Image processedImage;
     try {
       if (shouldApplyInvertedColorWithBlackAndWhite) {
-        processedImage = applyInvertedColor(applyBlackAndWhite(image));
+        processedImage = ImageProcessing.applyInvertedColor(
+            ImageProcessing.applyBlackAndWhite(image));
       } else if (shouldApplyBlackAndWhite) {
-        processedImage = applyBlackAndWhite(image);
+        processedImage = ImageProcessing.applyBlackAndWhite(image);
       } else if (shouldApplyInvertedColor) {
-        processedImage = applyInvertedColor(image);
+        processedImage = ImageProcessing.applyInvertedColor(image);
       } else {
         processedImage = image;
       }
       if (shouldApplyBillboard) {
-        processedImage = applyBillboard(image);
-      } else {
-        processedImage = image;
+        processedImage = ImageProcessing.applyBillboard(image);
       }
       if (shouldApplySepia) {
-        processedImage = applySepia(image);
-      } else {
-        processedImage = image;
+        processedImage = ImageProcessing.applySepia(image);
       }
       if (shouldApplyVignette) {
-        if (isNumeric(words.last)) {
-          print(isNumeric(words.last).toString());
-          print(double.tryParse(words.last).toString());
-          processedImage = applyVignette(image, double.tryParse(words.last));
+        if (ImageProcessing.isNumeric(words.last)) {
+          processedImage = ImageProcessing.applyVignette(
+              processedImage, double.tryParse(words.last)!);
+        } else {
+          processedImage = ImageProcessing.applyVignette(processedImage, 1.4);
         }
-        print(isNumeric(words.last).toString());
-        print(double.tryParse(words.last).toString());
-        processedImage = applyVignette(processedImage, 1.4);
-      } else {
-        processedImage = image;
       }
       if (shouldApplyBulge) {
-        processedImage = appyBulge(image);
-      } else {
-        processedImage = image;
+        processedImage = ImageProcessing.appyBulge(image);
       }
 
-      // Envia a imagem processada de volta para o processo principal.
       final sendPort = args['sendPort'] as SendPort;
       sendPort.send(processedImage);
     } catch (e) {
