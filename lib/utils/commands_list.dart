a25133a43b7f102a10046e7d36e6e72a8d74ad19
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2023-05-01 13:52:57 -0300

    commands list update

--- lib/utils/commands_list.dart
+++ lib/utils/commands_list.dart
@@ -6,11 +6,12 @@ billboard: Apply a billboard (the image is made up of dots) filter to the image\
 sepia: Apply a sepia (shades of yellow) filter to the image\n
 bulge: Apply a bulge effect to the image [-x, -y, -radius]\n
 vignette: Apply a vignette to the image (for custom values, use a hyphen "&make -vignette 1.3")\n
-gaussian: Apply a blur effect to the image [-blur]''';
+gaussian: Apply a blur effect to the image [-blur]
+emboss: Apply a emboss convolution filter to the given image''';
 
 String updates = '''
 Now the "bulge" command has three options to manipulate the distortion shape. They are: x, y, and radius.\n
-New command!\nType "gaussian" to make a blur in the image.
+Two new commands!\nType "gaussian" to make a blur in the image or "emboss" to make a emboss convolution.
 The "gaussian" command has one option to manipulate the blur. Type -blur <value> after "gaussian".\n
 To use these new commands, it's necessary to add a hyphen before the option, for example: 
 "&make bulge -x 10 -y 100 -radius 50 <image link>"
