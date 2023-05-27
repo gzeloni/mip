6be3848b1436bf33e1ae847b3fe71ed6527dc3c7
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2023-05-26 22:40:42 -0300

    &gif command bug fix in term String

--- lib/models/bot_commands/commands.dart
+++ lib/models/bot_commands/commands.dart
@@ -177,7 +177,7 @@ class BotCommands {
             // If offensive words are found, send an error message and return
             try {
               await event.message.channel.sendMessage(MessageBuilder.content(
-                  'Você não pode pesquisar usando esses termos!'));
+                  'Você não pode pesquisar usando esses terms!'));
             } catch (e) {
               sendEmbedMessageErrorHandler(e, event, bot);
             }
@@ -195,8 +195,9 @@ class BotCommands {
             } else {
               // Perform the GIF search and send a random GIF
               try {
-                String termos = words.sublist(1).join(' ');
-                await getBartGifs(termos);
+                String terms = words.sublist(1).join('-');
+                print(terms);
+                await getGifs(terms);
                 var randomItem = (gifUrls..shuffle()).first;
                 await event.message.channel
                     .sendMessage(MessageBuilder.content(randomItem.toString()));
