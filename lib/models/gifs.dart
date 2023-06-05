3a6f4ff392c18dc5aab3a6a83eec0ed19af9daed
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2023-06-05 20:27:51 -0300

    errors handling and documentation

--- lib/models/gifs.dart
+++ lib/models/gifs.dart
@@ -1,25 +1,32 @@
 import 'package:multithreading_image_processor/config/config.dart';
-import 'package:multithreading_image_processor/data/urls_list.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:multithreading_image_processor/data/urls_list.dart';
+
+// Function to fetch GIFs from the Giphy API
 Future<List<dynamic>> getGifs(String searchTerm) async {
   String apiKey = Config.getGiphyToken();
   String url =
       'https://api.giphy.com/v1/gifs/search?q=$searchTerm&api_key=$apiKey&limit=10';
-
-  await http.get(Uri.parse(url)).then((response) {
+  try {
+    var response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
+      // Decode the response body
       Map<String, dynamic> data = json.decode(response.body);
       List<dynamic> gifs = data['data'];
+
+      // Extract the GIF URLs from the response and add them to the list
       for (var gif in gifs) {
         String gifUrl = gif['images']['original']['url'];
         gifUrls.add(gifUrl);
       }
     } else {
-      print('Erro ao buscar gifs: ${response.statusCode}');
+      print('Error fetching GIFs: ${response.statusCode}');
     }
-  });
+  } catch (e) {
+    print('Error fetching GIFs: $e');
+  }
 
   return gifUrls;
 }
