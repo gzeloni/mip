18310d380e08178142260f893d09d17aed078bbe
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2023-06-05 19:40:15 -0300

    errors handling and documentation

--- lib/mip.dart
+++ lib/mip.dart
@@ -1,11 +1,11 @@
-/// This code is a Dart class called Mip that stands for Multithreading Image Processor.
-/// It includes a mip method that processes an image from a given link and saves it to a given file name.
-/// The processing can be customized with different options, such as applying a black and white filter,
-// an inverted color filter, a vignette effect, a billboard effect, a sepia filter, and a bulge distortion effect.
+/// This code is a Dart class called MultithreadingImageProcessor that implements a multithreading image processing functionality.
+/// The class includes a method called mip, which processes an image from a given link and saves it to a specified file name.
+/// The processing options can be customized, such as applying a black and white filter, inverted color filter, vignette effect,
+/// billboard effect, sepia filter, and bulge distortion effect.
 
-/// The class also includes a static imageProcessor method that processes the image in a separate isolate,
-// which allows for multithreading and improves performance.
-/// The words parameter in the constructor is a list of strings that represent the different processing options.
+/// The class also includes a static method called imageProcessor that processes the image in a separate isolate,
+/// allowing for multithreading and improved performance.
+/// The 'words' parameter in the constructor is a list of strings that represents the different processing options.
 
 import 'dart:io';
 import 'dart:isolate';
@@ -13,11 +13,14 @@ import 'package:image/image.dart' as img;
 import 'package:multithreading_image_processor/constants/options.dart';
 import 'package:multithreading_image_processor/models/filters.dart';
 import 'package:http/http.dart' as http;
+import 'package:multithreading_image_processor/models/log_function.dart';
 
-class Mip {
+class MultithreadingImageProcessor {
   final List<String> words;
-  Mip({required this.words});
 
+  MultithreadingImageProcessor({required this.words});
+
+  /// Method that processes an image using multithreading and saves it to a file.
   Future<void> mip(String imageLink, String filename) async {
     try {
       final uri = Uri.parse(imageLink);
@@ -37,109 +40,122 @@ class Mip {
         },
       );
       final newImage = await receivePort.first as img.Image;
-      await File(filename).writeAsBytes(
-          isGif == true ? img.encodeGif(newImage) : img.encodePng(newImage));
-    } on IsolateSpawnException catch (isolateSpawnException) {
-      print(isolateSpawnException);
-    } on RemoteError catch (remoteError) {
-      print(remoteError);
+
+      // Write the processed image to a file
+      try {
+        final file = File(filename);
+        await file.writeAsBytes(
+          isGif ? img.encodeGif(newImage) : img.encodePng(newImage),
+        );
+      } on FileSystemException catch (e) {
+        logError(
+            'Error in file system while saving the image: $e', 'OS_errors.log');
+      } catch (e) {
+        logError('Unknown error while saving the image: $e', 'OS_errors.log');
+      }
+    } on IsolateSpawnException catch (e) {
+      logError('Error creating the isolate: $e', 'isolate_errors.log');
+    } on RemoteError catch (e) {
+      logError('Remote error: $e', 'isolate_errors.log');
     } catch (e) {
-      print(e.toString());
+      logError('Unknown error: $e', 'unknown_errors.log');
     }
   }
 
-  // Method that processes the image in a separate isolate
+  /// Method that processes the image in a separate isolate.
   static void imageProcessor(Map<String, dynamic> args) async {
     final image = args['image'] as img.Image;
     final words = args['words'] as List<String>;
 
     // Parse the options from the list of words
     for (final word in words) {
-      // check if word starts with '-' to identify an option
+      // Check if the word starts with '-' to identify an option
       if (word.startsWith('-')) {
         final key = word.substring(1);
         bool index = words.indexOf(word) + 1 < words.length;
         bool isNumeric =
             ImageProcessing.isNumeric(words[words.indexOf(word) + 1]);
+
         if (key == 'x' && index && isNumeric) {
-          // if option is 'x' and the next word is a number, parse it to int and save it as centerX
+          // If the option is 'x' and the next word is a number, parse it to int and save it as centerX
           centerX = int.parse(words[words.indexOf(word) + 1]);
           shouldApply[key] = true;
         } else if (key == 'y' && index && isNumeric) {
-          // if option is 'y' and the next word is a number, parse it to int and save it as centerY
+          // If the option is 'y' and the next word is a number, parse it to int and save it as centerY
           centerY = int.parse(words[words.indexOf(word) + 1]);
           shouldApply[key] = true;
         } else if (key == 'radius' && index && isNumeric) {
-          // if option is 'radius' and the next word is a number, parse it to double and save it as radius
+          // If the option is 'radius' and the next word is a number, parse it to double and save it as radius
           radius = double.tryParse(words[words.indexOf(word) + 1]);
           shouldApply[key] = true;
         } else if (key == 'blur' && index && isNumeric) {
-          // if option is 'blur' and the next word is a number, parse it to double and save it as radius
+          // If the option is 'blur' and the next word is a number, parse it to double and save it as gaussRadius
           gaussRadius = int.parse(words[words.indexOf(word) + 1]);
           shouldApply[key] = true;
         } else if (key == 'shift' && index && isNumeric) {
-          // if option is 'shift' and the next word is a number, parse it to double and save it as radius
+          // If the option is 'shift' and the next word is a number, parse it to double and save it as shift
           shift = int.parse(words[words.indexOf(word) + 1]);
           shouldApply[key] = true;
-        } else if (shouldApply.containsKey(key)) {
-          // if option is valid and doesn't require a value, set its value to true
+        } else if (shouldApply[key] != null) {
+          // If the option is valid and doesn't require a value, set its value to true
           shouldApply[key] = true;
+
           if (key == 'vignette' && index && isNumeric) {
-            // if option is 'vignette' and the next word is a number, parse it to double and save it as vignetteThickness
+            // If the option is 'vignette' and the next word is a number, parse it to double and save it as vignetteThickness
             vignetteThickness = double.tryParse(words[words.indexOf(word) + 1]);
           }
         } else if (!isNumeric) {
           return;
         }
       } else if (shouldApply.containsKey(word)) {
-        // if word is a valid option and doesn't require a value, set its value to true
+        // If the word is a valid option and doesn't require a value, set its value to true
         shouldApply[word] = true;
       }
     }
 
-    ///This section of code takes in the processed image and applies image processing filters based on the shouldApply map.
-    ///The shouldApply map is a Map<String, bool> which represents which filters should be applied to the image.
+    // This section of code takes the processed image and applies image processing filters based on the shouldApply map.
+    // The shouldApply map is a Map<String, bool> which represents which filters should be applied to the image.
 
     img.Image processedImage = image;
 
-    // Check if 'p&b' flag is enabled
+    // Check if the 'p&b' flag is enabled
     if (shouldApply['p&b']!) {
-      // Apply black and white filter to the image
+      // Apply the black and white filter to the image
       processedImage =
           ImageProcessing.applyBlackAndWhite(image, image.hasAnimation);
     }
 
-    // Check if 'inverted' flag is enabled
+    // Check if the 'inverted' flag is enabled
     if (shouldApply['inverted']!) {
-      // Apply inverted color filter to the image
+      // Apply the inverted color filter to the image
       processedImage =
           ImageProcessing.applyInvertedColor(image, image.hasAnimation);
     }
 
-    // Check if 'billboard' flag is enabled
+    // Check if the 'billboard' flag is enabled
     if (shouldApply['billboard']!) {
-      // Apply billboard filter to the image
+      // Apply the billboard filter to the image
       processedImage =
           ImageProcessing.applyBillboard(processedImage, image.hasAnimation);
     }
 
-    // Check if 'sepia' flag is enabled
+    // Check if the 'sepia' flag is enabled
     if (shouldApply['sepia']!) {
-      // Apply sepia filter to the image
+      // Apply the sepia filter to the image
       processedImage =
           ImageProcessing.applySepia(processedImage, image.hasAnimation);
     }
 
-    // Check if 'vignette' flag is enabled
+    // Check if the 'vignette' flag is enabled
     if (shouldApply['vignette']!) {
-      // Apply vignette filter to the image
+      // Apply the vignette filter to the image
       processedImage = ImageProcessing.applyVignette(
           processedImage, vignetteThickness ?? 1.4, image.hasAnimation);
     }
 
-    // Check if 'bulge' flag is enabled
+    // Check if the 'bulge' flag is enabled
     if (shouldApply['bulge']!) {
-      // Apply bulge filter to the image
+      // Apply the bulge filter to the image
       processedImage = ImageProcessing.appyBulge(
           processedImage,
           centerX: centerX,
@@ -148,37 +164,37 @@ class Mip {
           image.hasAnimation);
     }
 
-    // Check if 'gaussian' flag is enabled
+    // Check if the 'gaussian' flag is enabled
     if (shouldApply['gaussian']!) {
-      // Apply gaussian filter to the image
+      // Apply the gaussian filter to the image
       processedImage = ImageProcessing.applyGaussianBlur(
           processedImage, gaussRadius ?? 5, image.hasAnimation);
     }
 
-    // Check if 'emboss' flag is enabled
+    // Check if the 'emboss' flag is enabled
     if (shouldApply['emboss']!) {
-      // Apply emboss convolution filter to the image
+      // Apply the emboss convolution filter to the image
       processedImage =
           ImageProcessing.applyEmboss(processedImage, image.hasAnimation);
     }
 
-    // Check if 'sobel' flag is enabled
+    // Check if the 'sobel' flag is enabled
     if (shouldApply['sobel']!) {
-      // Apply sobel edge detection filtering to the image
+      // Apply the sobel edge detection filtering to the image
       processedImage =
           ImageProcessing.applySobel(processedImage, image.hasAnimation);
     }
 
-    // Check if 'sketch' flag is enabled
+    // Check if the 'sketch' flag is enabled
     if (shouldApply['sketch']!) {
-      // Apply sketch filter to the image
+      // Apply the sketch filter to the image
       processedImage =
           ImageProcessing.applySketch(processedImage, image.hasAnimation);
     }
 
-    // Check if 'chromatic' flag is enabled
+    // Check if the 'chromatic' flag is enabled
     if (shouldApply['chromatic']!) {
-      // Apply chromatic aberration filter to the image
+      // Apply the chromatic aberration filter to the image
       processedImage = ImageProcessing.applyChromatic(
           processedImage, shift ?? 5, image.hasAnimation);
     }
