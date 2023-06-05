c460d5481310c7a5369270720c66a5291d526a9d
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2023-06-05 20:28:12 -0300

    update

--- lib/models/log_function.dart
+++ lib/models/log_function.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
-
 import 'package:nyxx/nyxx.dart';
 
+// Function to log errors to a file
 Future<void> logError(dynamic e, String filename) async {
   final file = File(filename);
   final sink = file.openWrite(mode: FileMode.append);
@@ -10,13 +10,18 @@ Future<void> logError(dynamic e, String filename) async {
   await sink.close();
 }
 
+// Function to handle and send error messages as an embed
 Future<void> sendEmbedMessageErrorHandler(
     Object e, IMessageReceivedEvent event, INyxxWebsocket bot) async {
+  // Fetch the owner of the bot
   final owner = await bot.fetchUser(Snowflake(event.message.author.id));
+
   try {
+    // Send an error message to the bot owner
     await owner.sendMessage(MessageBuilder.content(
         'Não tenho permissão de enviar mensagens no canal <#${event.message.channel.id}>'));
   } catch (e) {
-    await logError(e, 'error.log');
+    // If sending the error message fails, log the error to a file
+    await logError(e, 'errors/discord_errors.log');
   }
 }
