f7be735c53e919076d3508283fb0c62689c6c38c
Gustavo Zeloni <98529871+gzeloni@users.noreply.github.com>
2023-06-05 21:07:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir assets/ lib/config/
+mkdir assets/ lib/config/ errors/
 touch lib/utils/text_list.dart lib/config/config.dart
 echo "List textList = ['put some words here',];String randomText(){var randomItem = (textList..shuffle()).first;return randomItem.toString();}" > lib/utils/text_list.dart
 echo "class Config{static const String _discordToken='your discord token here';static const String _giphyToken='your gif token here';static String getDiscordToken(){return _discordToken;}static String getGiphyToken(){return _giphyToken;}}" > lib/config/config.dart
