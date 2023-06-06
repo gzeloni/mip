42e9a8f2347e6ddeb3a2c963329f49eaaa840628
Gustavo Zeloni <98529871+gzeloni@users.noreply.github.com>
2023-06-05 21:05:53 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mkdir mip/assets mip/lib/config
-touch mip/lib/utils/text_list.dart mip/lib/config/config.dart
-echo "List textList = ['put some words here',];String randomText(){var randomItem = (textList..shuffle()).first;return randomItem.toString();}" > mip/lib/utils/text_list.dart
-echo "class Config{static const String _discordToken='your discord token here';static const String _giphyToken='your gif token here';static String getDiscordToken(){return _discordToken;}static String getGiphyToken(){return _giphyToken;}}" > mip/lib/config/config.dart
-dart pub get -C mip
+mkdir assets/ lib/config/
+touch lib/utils/text_list.dart lib/config/config.dart
+echo "List textList = ['put some words here',];String randomText(){var randomItem = (textList..shuffle()).first;return randomItem.toString();}" > lib/utils/text_list.dart
+echo "class Config{static const String _discordToken='your discord token here';static const String _giphyToken='your gif token here';static String getDiscordToken(){return _discordToken;}static String getGiphyToken(){return _giphyToken;}}" > lib/config/config.dart
+dart pub get
 echo "EDIT THE FILES: [mip/lib/config/config.dart] AND [mip/lib/utils/text_list.dart]"
